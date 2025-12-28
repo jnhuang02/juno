a45a032ab31d643a543d57aee648341c851d9ecf
Justin Huang <justinstarwar@gmail.com>
2025-12-27 19:53:03 -0800

    stuff

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
-import App from "./App";
-import "./index.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter> 
-    <App />
-  </BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
