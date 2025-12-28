8b669383f74f2f0ec4dfd29cfc781721cb6e557d
Justin Huang <justinstarwar@gmail.com>
2025-12-28 00:21:36 -0800

    notfound

--- my-app/src/components/notfound.jsx
+++ my-app/src/components/notfound.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import "./notfound.scss";
 
 const NotFound = () => {
   return (
-    <h4 className="not-found">
+    <h4 className="mt-16 text-center text-2xl font-semibold text-red-400">
       Sorry, but the page you have requested was not found.
     </h4>
-  )
+  );
 };
 
 export default NotFound;
