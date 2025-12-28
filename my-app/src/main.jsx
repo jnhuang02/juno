e6e29bfccefefe4da797366332aa3a145166ba05
Justin Huang <justinstarwar@gmail.com>
2025-12-27 19:56:14 -0800

    fix

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './App';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App"; // <-- default import instead of { App }
+import "./index.css";
 
-ReactDOM.render(
+// create a root using React 18+/19 API
+const rootElement = document.getElementById("root");
+// (optional guard) if (!rootElement) throw new Error('Root element #root not found');
+
+createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/juno">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
